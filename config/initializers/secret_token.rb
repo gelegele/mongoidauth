a675360c76daf222fa9291c170cee31997709e57
U-ISHIBASHI-PC\Administrator <Administrator@ishibashi-pc.cad.css.fujitsu.com>
2012-12-03 18:30:21 +0900

    rails new mongoidauth  --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongoidauth::Application.config.secret_token = '229636c210007d557ca601b5987e33eccf9967b0d72de39e69c4959942d45d6aa000de607e5f0ef42394b138d30b04ab9bfac2953b1b507029b4a1c5fd59999a'
